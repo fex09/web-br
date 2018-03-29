8af102c89e39f96012b58d9dca84cf4ce4c26959
Fenix <fsolisg@gmail.com>
2018-03-28 18:13:21 -0600

    semana 7 avances

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,10 +1,12 @@
 var config ={
     distRoot: 'dist',
+
     server: {
         port:8000,
         root:'./dist',
         livereload: true
     },
+
     copy: {
         index:{
             src:'./src/index.html',
@@ -14,6 +16,23 @@ var config ={
             src: './src/assets/img/**/*',
             dest: './dist/assets/img'
         }
+    },
+
+    watch:{
+        index: ['./src/index.html','./src/templates/*.html'],
+        scss: './src/scss/**/*.scss'
+    },
+
+    sass:{
+        src: './src/scss/main.scss',
+        dest: './dist/assets/css',
+        style: 'compressed'
+    },
+
+    html:{
+        src:['./src/index.html', './src/templates/*.html'],
+        dest: './dist',
+        basePath: './src/templates/'
     }
 };
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,76 @@ const gulp = require('gulp');
 const connect = require('gulp-connect');
 const del = require('del');
 const runSequence = require('run-sequence');
+//const open = require('gulp-open'); para que abra el explorardor al correr el server
+const sass = require('gulp-sass');
+const htmlPartial = require('gulp-html-partial');
 
 //development serve task
-const serve = () => connect.server(config.server);
+const serve = () => {
+    connect.server(config.server);
+};
+
+
+//HTML partial taks
+const htmlTask = () => {
+    const htmlConfig = config.html;
+
+    return gulp.src(htmlConfig.src)
+        .pipe(htmlPartial({
+            basePath: htmlConfig.basePath
+        }))
+        .pipe(gulp.dest(htmlConfig.dest))
+        .pipe(connect.reload());
+};
+
+//SASS task
+const sassTask = () => {
+    const sassConfig = config.sass;
+
+    return gulp.src(sassConfig.src)
+        .pipe(sass({
+            outputStyle: sassConfig.style
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(sassConfig.dest))
+        .pipe(connect.reload());
+};
+
 
 //copu the files to distribution folder
 const copy = () => {
     const copyConfig = config.copy;
-    gulp.src(copyConfig.index.src)
-    .pipe(gulp.dest(copyConfig.index.dest));
+    /*gulp.src(copyConfig.index.src)
+    .pipe(gulp.dest(copyConfig.index.dest));*/
 
     return gulp.src(copyConfig.img.src)
         .pipe(gulp.dest(copyConfig.img.dest))
+        .pipe(connect.reload()); // recarga el servidor después de que copia.
 };
+
+
+const watch = () => {
+    const watchConfig = config.watch;
+    gulp.watch(watchConfig.index, ['copy']);
+    gulp.watch(watchConfig.sass,['sass']);
+}
+
 
 //build the project
 const build = () => del([config.distRoot]).then(()=>{
-    runSequence('copy');
+    runSequence('copy','sass','htmlTask');
 });
 
 const defaultTask = () => runSequence(
     'build',
-    'serve'
+    'serve',
+    'watch'
 )
 
 gulp.task('serve',serve);
 gulp.task('copy',copy);
 gulp.task('build',build);
+gulp.task('watch',watch);
+gulp.task('sass',sassTask);
+gulp.task('html',htmlTask);
 gulp.task('default',defaultTask);
 //gulp.task('default',() => console.log('Funciona!', config));
